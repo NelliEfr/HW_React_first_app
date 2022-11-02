45b97bccf984c490e98451e42d5200d43b874bfd
Evgeny <bril424@mail.ru>
2022-11-02 10:30:45 +0100

    add app

--- my_app/src/componenst/App/index.jsx
+++ my_app/src/componenst/App/index.jsx
@@ -3,8 +3,7 @@ import { workers } from '../../data/workers'
 
 export default function App() {
 
-  // const active_workers = workers.filter(el => el.in_office)
-
+ 
   return (
     <div>
       <Cards workers={workers}/>
